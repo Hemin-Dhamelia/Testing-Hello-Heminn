86ec67be262c2b39542587a3dcc57033f2bab531
HP <HP@localhost>
2024-09-12 20:57:36 -0400

    Second commit for Hello Heminn

--- src/testing/hello/hemin/TestingHelloHemin.java
+++ src/testing/hello/hemin/TestingHelloHemin.java
@@ -12,7 +12,8 @@ public class TestingHelloHemin {
 
     public static void main(String[] args){
         System.out.println("Hello Heminn");
+        System.out.println("This is the second push for Hello Hemin");
     }
     
 }
-// hemin dhamelia
+// hemin dhamelia 2.0
